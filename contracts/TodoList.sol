900a3f78637403f6c86c5ee407bb7f022dd95e7d
anchengyang <anchengyang72@gmail.com>
2022-12-28 02:44:35 +0800

    Add client side application (contains error)

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,22 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.5.0;
 
 contract TodoList{
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("my task");
+    }
+
+    function createTask(string memory content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, content, false);
+    }
 }
