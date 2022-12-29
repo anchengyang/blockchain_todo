9acb178fa4a35404b140cc5dee858786db07dc99
anchengyang <anchengyang72@gmail.com>
2022-12-29 22:48:32 +0800

    Create and complete task, added test cases

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,12 +11,34 @@ contract TodoList{
 
     mapping(uint => Task) public tasks;
 
+    // Event in solidity is used to log the transactions happening in the blockchain.
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
     constructor() public {
-        createTask("my task");
+        createTask("my first task");
     }
 
     function createTask(string memory content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, content, false);
+        // triggering an event to indicate that the create task function was called
+        emit TaskCreated(taskCount, content, false);
     }
+
+    function toggleCompleted(uint id) public {
+        Task memory task = tasks[id];
+        task.completed = !task.completed;
+        tasks[id] = task;
+        emit TaskCompleted(id, task.completed);
+    }
+
 }
